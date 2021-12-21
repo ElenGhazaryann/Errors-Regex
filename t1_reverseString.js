b590b18fd69c242bd43eed75b161ec5997685c61
Elen <elen.ghazaryan6@gmail.com>
2021-12-21 13:23:11 +0400

    fix: fix some bugs

--- t1_reverseString.js
+++ t1_reverseString.js
@@ -1,10 +1,11 @@
 "use strict";
+
 function reverseString(s) {
   try {
-    return s.split("").reverse().join();
+    console.log(s.split("").reverse().join(""));
   } catch (error) {
     console.log(error.message);
+    return s;
   }
-  return s;
 }
-console.log(reverseString(1234));
+console.log(reverseString("1234"));
